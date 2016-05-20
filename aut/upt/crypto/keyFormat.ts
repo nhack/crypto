af378b7d59665c96eca4facc92807cf8d615a381
Sorin Guga <sorin.guga@e-spres-oh.com>
2016-05-20 10:30:51 +0300

    Fixed string enum shimming for KeyFormat and KeyUsage.

--- aut/upt/crypto/keyFormat.ts
+++ aut/upt/crypto/keyFormat.ts
@@ -1,6 +1,12 @@
-export const KeyFormat = {
-  RAW: 'raw',
-  PKCS8: 'pkcs8',
-  SPKI: 'spki',
-  JWK: 'jwk'
+export class KeyFormat {
+  constructor (public val: string){}
+
+  toString() {
+    return this.val;
+  }
+
+  static RAW = 'raw';
+  static PKCS8 = 'pkcs8';
+  static SPKI = 'spki';
+  static JWK = 'jwk';
 }
--- aut/upt/crypto/keyUsage.ts
+++ aut/upt/crypto/keyUsage.ts
@@ -1,10 +1,17 @@
-export const KeyUsage = {
-  ENCRYPT: "encrypt",
-  DECRYPT: "decrypt",
-  SIGN: "sign",
-  VERIFY: "verify",
-  DERIVE_KEY: "deriveKey",
-  DERIVE_BITS: "deriveBits",
-  WRAP_KEY: "wrapKey",
-  UNWRAP_KEY: "unwrapKey"
+export class KeyUsage {
+  constructor(public val: string){
+  }
+
+  toString() {
+    return this.val;
+  }
+
+  static ENCRYPT = "encrypt";
+  static DECRYPT = "decrypt";
+  static SIGN = "sign";
+  static VERIFY = "verify";
+  static DERIVE_KEY = "deriveKey";
+  static DERIVE_BITS = "deriveBits";
+  static WRAP_KEY = "wrapKey";
+  static UNWRAP_KEY = "unwrapKey";
 }
